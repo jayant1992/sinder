631b70c2d8f05456ec04ee02ba49b4f12a53293a
Jayant Jain <jayantjain1992@gmail.com>
2015-06-29 13:25:53 +0530

    Adds script for seed data, model associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# 5.times do |i|
+#   Release.create name: "Release ##{i+1}"
+# end
+begin
+    db = SQLite3::Database.new "datasets/track_metadata.db"
+    fields = ["title", "artist_name", "artist_mbid", "release", "year"]
+    
+    stm = db.prepare "SELECT " + fields.join(", ") + " FROM songs WHERE artist_familiarity > 0.95" 
+    rs = stm.execute 
+    
+    artists = {}
+    releases = {}
+    rs.each do |row|
+    	if artists.has_key? row[1]
+    		artist = artists[row[1]]
+    	else
+    		artist = Artist.create name: row[1], mb_id: row[2]
+    		artists[row[1]] = artist
+    	end
+
+		if releases.has_key? row[3]
+    		release = releases[row[3]]
+    	else
+    		release = Release.create name: row[3]
+    		releases[row[3]] = release
+		end
+
+		artist.songs.create title: row[0], release_id: release.id, year: row[4]
+    end
+
+rescue SQLite3::Exception => e 
+    puts "Exception occurred"
+    puts e
+    
+ensure
+	stm.close if stm
+    db.close if db
+end
