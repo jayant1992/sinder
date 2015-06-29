7cb4d027b22d848494a7d5db13aee3857da4d4b5
Saurabh Kumar <saurabh.kumar@housing.com>
2015-06-30 03:05:19 +0530

    Seed database with tags
    Tags are obtained from MusicBrainz

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,31 +8,86 @@
 # 5.times do |i|
 #   Release.create name: "Release ##{i+1}"
 # end
+
+require 'musicbrainz'
+require 'rest-client'
+
+def getmbtags(row)
+
+  tags = []
+  recording = (MusicBrainz::Recording.search(row[0], row[1]))[0]
+
+  unless recording.blank?
+    trackmbid = recording[:id]
+
+    response = RestClient.get "http://musicbrainz.org/ws/2/recording?query=#{row[0]} AND arid:#{row[2]}&fmt=json"
+    case response.code
+    when 200
+      resp_hash = JSON.parse response
+      resp_hash["recordings"].each do |resp|
+        unless resp["tags"].blank?
+          resp["tags"].each {|tag| tags << tag["name"] }
+        end
+      end
+    else
+      return []
+    end
+    tags.uniq!
+    # MB rate limits
+    sleep(1.0)
+    return tags
+  end
+  return []
+end
+
 begin
+
+  MusicBrainz.configure do |c|
+    # Application identity (required)
+    c.app_name = "Sinder"
+    c.app_version = "1.0"
+    c.contact = "saurabh.kumar@housing.com"
+
+    # Cache config (optional)
+    c.cache_path = "/tmp/musicbrainz-cache"
+    c.perform_caching = true
+
+    # Querying config (optional)
+    c.tries_limit = 2
+  end
+
   db = SQLite3::Database.new "datasets/track_metadata.db"
   fields = ["title", "artist_name", "artist_mbid", "release", "year"]
+
+  count = db.get_first_value( "SELECT COUNT(*) FROM songs WHERE artist_familiarity > 0.85 and artist_mbid!=''" )
   
-  stm = db.prepare "SELECT " + fields.join(", ") + " FROM songs WHERE artist_familiarity > 0.95" 
+  stm = db.prepare "SELECT " + fields.join(", ") + " FROM songs WHERE artist_familiarity > 0.85 and artist_mbid!=''" 
   rs = stm.execute 
   
   artists = {}
   releases = {}
-  rs.each do |row|
+  
+  rs.each_with_index do |row, index|
     if artists.has_key? row[1]
       artist = artists[row[1]]
     else
-      artist = Artist.create name: row[1], mb_id: row[2]
+      artist = Artist.find_or_create_by name: row[1], mb_id: row[2]
       artists[row[1]] = artist
     end
 
     if releases.has_key? row[3]
       release = releases[row[3]]
     else
-      release = Release.create name: row[3]
+      release = Release.find_or_create_by name: row[3]
       releases[row[3]] = release
     end
 
-    artist.songs.create title: row[0], release_id: release.id, year: row[4]
+    song = artist.songs.find_or_create_by title: row[0], release_id: release.id, year: row[4]
+
+    puts "Processing #{index} of #{count}."
+    tags = getmbtags row
+    tags.each { |tag| song.tag << Tag.find_or_create_by(:name => tag) }
+    
   end
 
 rescue SQLite3::Exception => e 
