8737dd3418233177cc7f5fd72cf972ca3172690f
Arpit Temani <temaniarpit27@gmail.com>
2015-07-01 12:53:36 +0530

    Adds id=null in dislikes and netural tables

--- db/migrate/20150701070635_create_join_table_songs_users_dislikes.rb
+++ db/migrate/20150701070635_create_join_table_songs_users_dislikes.rb
@@ -1,10 +1,8 @@
 class CreateJoinTableSongsUsersDislikes < ActiveRecord::Migration
     def change
-        create_table :songs_users_dislikes do |t|
+        create_table :songs_users_dislikes, :id => false do |t|
             t.references :song, :null => false
             t.references :user, :null => false
-            # t.index [:song_id, :user_id]
-            # t.index [:user_id, :song_id]
         end
     end
 end
--- db/migrate/20150701070754_create_join_table_songs_users_neutral.rb
+++ db/migrate/20150701070754_create_join_table_songs_users_neutral.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableSongsUsersNeutral < ActiveRecord::Migration
     def change
-        create_table :songs_users_neutral do |t|
+        create_table :songs_users_neutral, :id => false do |t|
             t.references :song, :null => false
             t.references :user, :null => false 
         end
